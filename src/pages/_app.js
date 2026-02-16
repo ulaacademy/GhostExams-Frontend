c29e6c7ec71d9265860fe27daa8901864a1e6aa0
Saeed Shahin <saeednshahin@gamil.com>
2026-02-16 14:58:41 +0300

    update: <اكتب وصف بسيط>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
